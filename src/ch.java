4cfb79b1de606866d942e12cdf66554af6fd9da9
BogVL30 <vova.bogdan.00@bk.ru>
2022-04-07 14:17:48 +0300

    Test

---
+++ src/ch.java
@@ -0,0 +1,11 @@
+public class ch {
+    public static void main(String[] args){
+        int x = 5;
+        String s = "Geloo";
+        String f = "Hello";
+        String g = ",";
+        String h = "HaY";
+        System.out.println(s+f+g+h);
+    }
+
+}
